5e8a3396c5886a8d29bb2f1d62abd808f0ab3003
Sepe19 <seba217i@stud.kea.dk>
2022-03-14 21:57:52 +0100

    first commit

--- src/components/1_introduction/1_Introduction.svelte
+++ src/components/1_introduction/1_Introduction.svelte
@@ -1,8 +1,7 @@
 <script>
-import HelloWorld from "../1.introduction/HelloWorld.svelte";
-
     import DynamicAttributes from "./DynamicAttributes.svelte";
     import HelloWorld from "./HelloWorld.svelte";
 </script>
 
-<DynamicAttributes></DynamicAttributes>
+<DynamicAttributes/>
+<HelloWorld/>
